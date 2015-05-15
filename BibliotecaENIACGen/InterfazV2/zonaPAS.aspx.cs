5a93315e83c33741e8b78fbd4c3c59478125b17b
projectslack <danielperezsansano@gmail.com>
2015-05-15 18:56:42 +0200

    Version interfaz Usuario PAS parcial

---
+++ BibliotecaENIACGen/InterfazV2/zonaPAS.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InterfazV2
+{
+    public partial class zonaPAS : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void asignarPrestamo(object sender, EventArgs e)
+        {
+
+        }
+        protected void verDesideratas(object sender, EventArgs e)
+        {
+
+        }
+        protected void nuevaObra(object sender, EventArgs e)
+        {
+
+        }
+        protected void modificarObra(object sender, EventArgs e)
+        {
+
+        }
+        protected void borrarObra(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
